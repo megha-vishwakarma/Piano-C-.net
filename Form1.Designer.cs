f89e2da60c9405e55aeee75d6f759b4115657f7b
megha-vishwakarma <70430389+megha-vishwakarma@users.noreply.github.com>
2021-12-22 06:36:47 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace piano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Anote = new System.Windows.Forms.Button();
+            this.Bnote = new System.Windows.Forms.Button();
+            this.Cnote = new System.Windows.Forms.Button();
+            this.Enote = new System.Windows.Forms.Button();
+            this.Dnote = new System.Windows.Forms.Button();
+            this.Fnote = new System.Windows.Forms.Button();
+            this.Gnote = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SandyBrown;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(804, 471);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Anote
+            // 
+            this.Anote.BackColor = System.Drawing.Color.White;
+            this.Anote.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Anote.Location = new System.Drawing.Point(18, 12);
+            this.Anote.Name = "Anote";
+            this.Anote.Size = new System.Drawing.Size(122, 300);
+            this.Anote.TabIndex = 1;
+            this.Anote.Text = "A";
+            this.Anote.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Anote.UseVisualStyleBackColor = false;
+            this.Anote.Click += new System.EventHandler(this.Anote_Click);
+            // 
+            // Bnote
+            // 
+            this.Bnote.BackColor = System.Drawing.Color.White;
+            this.Bnote.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bnote.Location = new System.Drawing.Point(146, 12);
+            this.Bnote.Name = "Bnote";
+            this.Bnote.Size = new System.Drawing.Size(122, 300);
+            this.Bnote.TabIndex = 2;
+            this.Bnote.Text = "B";
+            this.Bnote.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Bnote.UseVisualStyleBackColor = false;
+            this.Bnote.Click += new System.EventHandler(this.Bnote_Click);
+            // 
+            // Cnote
+            // 
+            this.Cnote.BackColor = System.Drawing.Color.White;
+            this.Cnote.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cnote.Location = new System.Drawing.Point(274, 12);
+            this.Cnote.Name = "Cnote";
+            this.Cnote.Size = new System.Drawing.Size(122, 300);
+            this.Cnote.TabIndex = 3;
+            this.Cnote.Text = "C";
+            this.Cnote.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Cnote.UseVisualStyleBackColor = false;
+            this.Cnote.Click += new System.EventHandler(this.Cnote_Click);
+            // 
+            // Enote
+            // 
+            this.Enote.BackColor = System.Drawing.Color.White;
+            this.Enote.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Enote.Location = new System.Drawing.Point(530, 12);
+            this.Enote.Name = "Enote";
+            this.Enote.Size = new System.Drawing.Size(122, 300);
+            this.Enote.TabIndex = 4;
+            this.Enote.Text = "E";
+            this.Enote.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Enote.UseVisualStyleBackColor = false;
+            this.Enote.Click += new System.EventHandler(this.Enote_Click);
+            // 
+            // Dnote
+            // 
+            this.Dnote.BackColor = System.Drawing.Color.White;
+            this.Dnote.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dnote.Location = new System.Drawing.Point(402, 12);
+            this.Dnote.Name = "Dnote";
+            this.Dnote.Size = new System.Drawing.Size(122, 300);
+            this.Dnote.TabIndex = 5;
+            this.Dnote.Text = "D";
+            this.Dnote.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Dnote.UseVisualStyleBackColor = false;
+            this.Dnote.Click += new System.EventHandler(this.Dnote_Click);
+            // 
+            // Fnote
+            // 
+            this.Fnote.BackColor = System.Drawing.Color.White;
+            this.Fnote.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Fnote.Location = new System.Drawing.Point(658, 12);
+            this.Fnote.Name = "Fnote";
+            this.Fnote.Size = new System.Drawing.Size(122, 300);
+            this.Fnote.TabIndex = 6;
+            this.Fnote.Text = "F";
+            this.Fnote.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Fnote.UseVisualStyleBackColor = false;
+            this.Fnote.Click += new System.EventHandler(this.Fnote_Click);
+            // 
+            // Gnote
+            // 
+            this.Gnote.BackColor = System.Drawing.Color.White;
+            this.Gnote.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gnote.Location = new System.Drawing.Point(786, 12);
+            this.Gnote.Name = "Gnote";
+            this.Gnote.Size = new System.Drawing.Size(122, 300);
+            this.Gnote.TabIndex = 7;
+            this.Gnote.Text = "G";
+            this.Gnote.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Gnote.UseVisualStyleBackColor = false;
+            this.Gnote.Click += new System.EventHandler(this.Gnote_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(113, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 155);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "A";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(370, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 155);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "A";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(633, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(54, 155);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "A";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.70909F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(914, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 300);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "G";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 549);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Gnote);
+            this.Controls.Add(this.Fnote);
+            this.Controls.Add(this.Dnote);
+            this.Controls.Add(this.Enote);
+            this.Controls.Add(this.Cnote);
+            this.Controls.Add(this.Bnote);
+            this.Controls.Add(this.Anote);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Piano";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Anote;
+        private System.Windows.Forms.Button Bnote;
+        private System.Windows.Forms.Button Cnote;
+        private System.Windows.Forms.Button Enote;
+        private System.Windows.Forms.Button Dnote;
+        private System.Windows.Forms.Button Fnote;
+        private System.Windows.Forms.Button Gnote;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace piano
+{
+    public partial class Form1 : Form
+    {       //https://www.youtube.com/watch?v=mQZavCCfvS4
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+        private void Anote_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(@"C:\Users\infom\source\repos\piano\piano\Resources\A.wav");
+            sp.Play();
+        }
+
+        private void Bnote_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(@"C:\Users\infom\source\repos\piano\piano\Resources\B.wav");
+            sp.Play();
+        }
+
+        private void Cnote_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(@"C:\Users\infom\source\repos\piano\piano\Resources\C.wav");
+            sp.Play();
+        }
+
+        private void Dnote_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(@"C:\Users\infom\source\repos\piano\piano\Resources\D.wav");
+            sp.Play();
+        }
+
+        private void Enote_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(@"C:\Users\infom\source\repos\piano\piano\Resources\E.wav");
+            sp.Play();
+        }
+
+        private void Fnote_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(@"C:\Users\infom\source\repos\piano\piano\Resources\F.wav");
+            sp.Play();
+        }
+
+        private void Gnote_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(@"C:\Users\infom\source\repos\piano\piano\Resources\G.wav");
+            sp.Play();
+        }
+    }
+}
